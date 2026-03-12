3a974a003b0c3b303ccc16ae6d15d6d8fa046b9a
Khắc Duy <188033594+D157D@users.noreply.github.com>
2026-03-12 09:46:48 +0700

    up

--- Assets/Crazy_Lobby/Script/NetWork/PlayerRunner.cs
+++ Assets/Crazy_Lobby/Script/NetWork/PlayerRunner.cs
@@ -1,14 +1,23 @@
 using Fusion;
 using UnityEngine;
 
-public class PlayerRunner : NetworkBehaviour, IPlayerJoined
+public class PlayerRunner : NetworkBehaviour, IPlayerJoined, IPlayerLeft
 {
-    public NetworkObject _player;
+    public NetworkPrefabRef _player;
     public void PlayerJoined(PlayerRef player)
     {
-        if(player == Runner.LocalPlayer)
+        if (Runner.IsServer)
         {
-            Runner.Spawn(_player, new Vector3(11, 0, 11), Quaternion.identity, player);
+            Vector3 spawnPosition = new Vector3(11, 0, 11) + new Vector3(Random.Range(-2f, 2f), 0, Random.Range(-2f, 2f));
+            Runner.Spawn(_player, spawnPosition, Quaternion.identity, player);
+        }
+    }
+
+    public void PlayerLeft(PlayerRef player)
+    {
+        if(Runner.IsServer)
+        {
+            Runner.Despawn(Runner.GetPlayerObject(player));
         }
     }
 }

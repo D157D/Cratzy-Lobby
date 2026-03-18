fa786b7ab3ef3f3f43656906b9dcc09bc90df663
Khắc Duy <188033594+D157D@users.noreply.github.com>
2026-03-18 16:45:32 +0700

    up

--- Assets/Crazy_Lobby/Script/Fall/FragilePlatform.cs
+++ Assets/Crazy_Lobby/Script/Fall/FragilePlatform.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class FragilePlatform : MonoBehaviour
 {
-    public int platformID; // Sẽ được MapManager gán tự động
+    public int platformID; 
     public float timeToBreak = 1.5f;
     private bool isBreaking = false;
     
@@ -17,36 +17,29 @@ public class FragilePlatform : MonoBehaviour
         propBlock = new MaterialPropertyBlock();
     }
 
-    // Hàm này được gọi bởi MapManager thông qua RPC
     public void StartBreakingLocally()
     {
-        if (isBreaking) return; // Nếu đang vỡ rồi thì thôi
+        if (isBreaking) return; 
         isBreaking = true;
         StartCoroutine(BreakRoutine());
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        // Kiểm tra nếu đối tượng đạp lên là người chơi (cần gán tag "Player" cho nhân vật)
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Tùy thuộc vào logic mạng (Multiplayer), bạn có thể gọi trực tiếp hàm này 
-            // hoặc gửi tín hiệu cho MapManager để MapManager gọi RPC đồng bộ cho mọi người.
             StartBreakingLocally();
         }
     }
 
     IEnumerator BreakRoutine()
     {
-        // Đổi màu vàng
         ChangeColor(Color.yellow);
         yield return new WaitForSeconds(timeToBreak / 2f);
 
-        // Đổi màu đỏ
         ChangeColor(Color.red);
         yield return new WaitForSeconds(timeToBreak / 2f);
 
-        // Tắt khối sàn (chỉ tắt render và collider)
         gameObject.SetActive(false);
     }
 
--- Assets/Crazy_Lobby/Script/Fall/MapManager.cs
+++ Assets/Crazy_Lobby/Script/Fall/MapManager.cs
@@ -5,24 +5,20 @@ public class MapManager : NetworkBehaviour
 {
     public static MapManager Instance;
     
-    // Kéo thả toàn bộ các khối sàn vào đây trên Inspector
     public FragilePlatform[] allPlatforms; 
 
     void Awake()
     {
         Instance = this;
-        // Đánh ID cho từng sàn để dễ gọi tên
         for (int i = 0; i < allPlatforms.Length; i++)
         {
             allPlatforms[i].platformID = i;
         }
     }
 
-    // Gửi lệnh qua mạng: Yêu cầu tất cả các máy (All) làm vỡ sàn có ID này
     [Rpc(RpcSources.All, RpcTargets.All)]
     public void RPC_TriggerPlatformBreak(int platformID)
     {
-        // Khi nhận được lệnh, tìm đúng cái sàn đó và bắt đầu đếm ngược ở MÁY LOCAL
         if (platformID >= 0 && platformID < allPlatforms.Length)
         {
             allPlatforms[platformID].StartBreakingLocally();

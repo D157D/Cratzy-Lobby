4fb53ec52a97b55dcb9d0cf614547c5116249107
Khắc Duy <188033594+D157D@users.noreply.github.com>
2026-03-18 20:15:18 +0700

    up

--- Assets/Crazy_Lobby/Script/Trap/SawTrap.cs
+++ Assets/Crazy_Lobby/Script/Trap/SawTrap.cs
@@ -32,11 +32,17 @@ namespace Crazy_Lobby.Trap
 
             if (other.TryGetComponent<NetworkCharacterController>(out var ncc))
             {
-                Vector3 knockbackDirection = (other.transform.position - transform.position).normalized;
-
+                Vector3 knockbackDirection = other.transform.position - transform.position;
                 knockbackDirection.y = 0;
 
-                ncc.Velocity += knockbackDirection * _knockbackForce;
+                if (knockbackDirection.sqrMagnitude < 0.001f)
+                {
+                    knockbackDirection = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+                }
+
+                Vector3 newVelocity = ncc.Velocity + (knockbackDirection.normalized * _knockbackForce);
+                newVelocity.y = 5f;
+                ncc.Velocity = newVelocity;
             }
         }
     }

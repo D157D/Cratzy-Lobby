a7c66c3a47ce24ca6f87c3bcd56ec3c90866e1dd
Khắc Duy <188033594+D157D@users.noreply.github.com>
2026-03-20 20:07:15 +0700

    Update Firework.cs

--- Assets/Crazy_Lobby/Script/Item/Firework.cs
+++ Assets/Crazy_Lobby/Script/Item/Firework.cs
@@ -7,14 +7,14 @@ namespace Cratzy_Lobby.Item
     public class Firework : Items
     {
         public NetworkPrefabRef _firework;
-        public Firework(string _ability, float _timer, string _des) : base(_ability, _timer, _des)
-        {
-            _ability = "Firework";
-            _timer = 5f;
-            _des = "Shoot a firework directly to other player";
-        }
+        public Firework() : base("Firework", 5f, "Shoot a firework directly to other player") { }
 
         public override void Use(Items item)
+        {
+            
+        }
+
+        private void SelectPlayer()
         {
             
         }

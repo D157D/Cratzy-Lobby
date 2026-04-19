d89d9afc4fea274df5c5f8558b3f620d8042b608
Vo Anh Quoc <kitaraaa122@gmail.com>
2026-04-19 23:20:59 +0700

    a

--- Assets/Scripts/RotPlatform_Up.cs
+++ Assets/Scripts/RotPlatform_Up.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class RotPlatform_Up : MonoBehaviour
 {
-    public float rotatingSpeed = 130;
+    public float rotatingSpeed = 130f;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up, rotatingSpeed * Time.deltaTime);
+    }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.transform.SetParent(null);
+        }
     }
 }
